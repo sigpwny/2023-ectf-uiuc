0411c7225db690786f7ddee41318435b6b376f68
Richard Liu <richy.liu.2002@gmail.com>
2023-02-14 22:52:37 -0600

    switch to k256 crate for crypto

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,12 @@
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use ed25519_dalek::{Keypair, Signer};
 use embedded_hal::digital::v2::OutputPin;
-use linked_list_allocator::LockedHeap;
-use rand_chacha;
-use rand_core;
-use rand_core::SeedableRng;
 
 use tiva::{driverlib, setup_board, Board};
 
-#[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
-
 #[entry]
 fn main() -> ! {
-    // Use the linked list allocator directory instead of embedded-alloc because
-    // that interferes with the C tiva driverlib linking (the critical_section
-    // crate has some segments which conflict).
-    //
-    // Ideally, this should be done in a critical section where interrupts are
-    // disabled.
-    {
-        use core::mem::MaybeUninit;
-        const HEAP_SIZE: usize = 0x4000;
-        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-        unsafe {
-            ALLOCATOR
-                .lock()
-                .init(HEAP_MEM.as_ptr() as *mut u8, HEAP_SIZE);
-        }
-    }
-
     let mut board: Board = setup_board();
 
     hprintln!("Hello, world!").unwrap();
@@ -44,6 +19,13 @@ fn main() -> ! {
 }
 
 fn crypto_example() {
+    // key generation
+    use rand_chacha::rand_core::SeedableRng;
+    // signing
+    use k256::ecdsa::{signature::Signer, Signature, SigningKey};
+    // verifying
+    use k256::ecdsa::{signature::Verifier, VerifyingKey};
+
     // The only time we need crypto on the device is:
     // 1. The car generates a random nonce, which the fob signs and the car verifies
     // 2. The car verifies that a feature was signed by the factory
@@ -52,12 +34,13 @@ fn crypto_example() {
     let mut rng = rand_chacha::ChaChaRng::from_seed([0; 32]);
 
     // keypair generation should be done on the host, but here's how to do it on the device
-    let keypair = Keypair::generate(&mut rng);
+    let signing_key = SigningKey::random(&mut rng); // Serialize with `::to_bytes()`
     let message: &[u8] = b"Some text";
-    let signature = keypair.sign(message);
+    let signature: Signature = signing_key.sign(message);
     // hprintln!("Signature: {:?}", signature).unwrap();
 
-    assert!(keypair.verify(message, &signature).is_ok());
+    let verifying_key = VerifyingKey::from(&signing_key); // Serialize with `::to_encoded_point()`
+    assert!(verifying_key.verify(message, &signature).is_ok());
     hprintln!("Signature verified!").unwrap();
 }
 
