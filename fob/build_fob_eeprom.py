0dad3ac2337e5f75a40b50a9b4927aebecd292b5
Minh Duong <TheWhiteHoodHacker@gmail.com>
2023-03-09 15:28:10 -0600

    Update for features

--- fob/build_fob_eeprom.py
+++ fob/build_fob_eeprom.py
@@ -11,15 +11,9 @@ secrets_dir = sys.argv[1]
 eeprom_file = sys.argv[2]
 
 # put the salted pin in eeprom if necessary
-fob_sec = open(os.path.join(secrets_dir, "fob_sec"), "rb").read()
-car_factory_sec = open(os.path.join(secrets_dir, "feat1_sig"), "rb").read()
-feat1 = os.urandom(4)
-feat2 = os.urandom(4)
-feat3 = os.urandom(4)
-
-
 
 if len(sys.argv) > 4:
+    fob_sec = open(os.path.join(secrets_dir, "fob_sec"), "rb").read()
     car_id = int(sys.argv[3], base=16).to_bytes(4, "big")
     pair_pin = bytes.fromhex(sys.argv[4])
 
@@ -34,24 +28,13 @@ if len(sys.argv) > 4:
     fob_sec_enc = m.digest()
     fob_sec_enc = bytes(a ^ b for a, b in zip(fob_sec_enc, fob_sec))
 
-    feat1_sig_a, feat1_sig_b = ecdsa.sign(car_id+feat1, int.from_bytes(car_factory_sec, "big"), curve.P256)
-    feat1_sig = (feat1_sig_a+feat1_sig_b).to_bytes(64, "big")
-    feat2_sig_a, feat2_sig_b = ecdsa.sign(car_id+feat2, int.from_bytes(car_factory_sec, "big"), curve.P256)
-    feat2_sig = (feat2_sig_a+feat2_sig_b).to_bytes(64, "big")
-    feat3_sig_a, feat3_sig_b = ecdsa.sign(car_id+feat3, int.from_bytes(car_factory_sec, "big"), curve.P256)
-    feat3_sig = (feat3_sig_a+feat3_sig_b).to_bytes(64, "big")
-
 
 else:
+    fob_sec = None
+    fob_sec_enc = None
     car_id = None
     hashed_pin = None
     salt = None
-    fob_sec_enc = None
-    feat1_sig = None
-    feat2_sig = None
-    feat3_sig = None
-
-
 
 addresses = {
     "FOBMEM_FOB_SECRET":     [0x100, fob_sec],
@@ -59,12 +42,12 @@ addresses = {
     "FOBMEM_FOB_SALT":       [0x140, salt],
     "FOBMEM_PIN_HASH":       [0x160, hashed_pin],
     "FOBMEM_CAR_ID":         [0x200, car_id],
-    "FOBMEM_FEAT_1":         [0x204, feat1],
-    "FOBMEM_FEAT_2":         [0x208, feat2],
-    "FOBMEM_FEAT_3":         [0x20C, feat3],
-    "FOBMEM_FEAT_1_SIG":     [0x240, feat1_sig],
-    "FOBMEM_FEAT_2_SIG":     [0x280, feat2_sig],
-    "FOBMEM_FEAT_3_SIG":     [0x2C0, feat3_sig],
+    "FOBMEM_FEAT_1":         [0x204, None],
+    "FOBMEM_FEAT_2":         [0x208, None],
+    "FOBMEM_FEAT_3":         [0x20C, None],
+    "FOBMEM_FEAT_1_SIG":     [0x240, None],
+    "FOBMEM_FEAT_2_SIG":     [0x280, None],
+    "FOBMEM_FEAT_3_SIG":     [0x2C0, None],
     "FOBMEM_CAR_PUBLIC":     [0x300, open(os.path.join(secrets_dir, "car_pub"), "rb").read()],
     "FOBMEM_FOB_IS_PAIRED":  [0x400, None],
     "FOBMEM_MSG_FEAT_3":     [0x700, None],
